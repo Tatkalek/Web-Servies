2f26c7c768279b4650f0885a53e24a2c671510b8
Tatka <dolinyak180199@mail.ru>
2019-09-14 20:05:48 +0400

    First commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout <<"¬ведите двухзначное целое число: "<< endl;
+	cin >> a;
+	cin >> b;
+
+	return 0;
+}
